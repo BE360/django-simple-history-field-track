bb12154f3c96262156506265d5d986a2916780a2
Reza Abbasi <rz.abs.shi@gmail.com>
2019-06-20 16:16:08 +0430

    bug fixed

--- simple_history_field_track/simple_history_decorators.py
+++ simple_history_field_track/simple_history_decorators.py
@@ -11,23 +11,23 @@ def should_record_history(wrapped_func):
 
             if self.pk:
                 query_set = HistoryManager(self._meta.model).get_queryset()
-                previous_state = query_set.get(id=self.pk)
+                previous_state = query_set.filter(pk=self.pk).first()
 
-                model_fields = [f.name for f in self._meta.get_fields()]
-                try:
-                    tracking_fields = [field.split('|')[1] for field in ModelsTrackingFields.objects.get(
-                        model_name=self._meta.model.__name__.lower()).tracking_fields]
+                if previous_state:
+                    model_fields = [f.name for f in self._meta.get_fields()]
+                    try:
+                        tracking_fields = [field.split('|')[1] for field in ModelsTrackingFields.objects.get(
+                            model_name=self._meta.model.__name__.lower()).tracking_fields]
 
-                    changed_fields = list(filter(
-                        lambda field: (field in tracking_fields) and getattr(self, field, None) != getattr(
-                            previous_state, field, None), model_fields))
-                    if len(changed_fields) == 0:
+                        changed_fields = list(filter(
+                            lambda field: (field in tracking_fields) and getattr(self, field, None) != getattr(
+                                previous_state, field, None), model_fields))
+                        if len(changed_fields) == 0:
+                            self.skip_history_when_saving = True
+                    except ModelsTrackingFields.DoesNotExist:
                         self.skip_history_when_saving = True
-                except ModelsTrackingFields.DoesNotExist:
-                    self.skip_history_when_saving = True
 
             new_args = list(args)
-
             new_args[0] = self
         else:
             new_args = args
